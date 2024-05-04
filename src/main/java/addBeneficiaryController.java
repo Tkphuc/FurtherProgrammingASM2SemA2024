f9a492912abc6eb2273d15cf8221d93a5d802b6f
Tran Kiem Phuc <s3927187@rmit.edu.vn>
2024-05-04 17:07:08 +0700

    Add beneficiary fxml and controller and update

---
+++ src/main/java/addBeneficiaryController.java
@@ -0,0 +1,26 @@
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/*https://www.youtube.com/watch?v=hcM-R-YOKkQ
+* */
+public class addBeneficiaryController {
+
+    public TextField beneficiaryTypeField;
+    public TextField beneficiaryNameField;
+    public TextField beneficiaryAddress;
+    public TextField beneficiaryEmail;
+    public TextField beneficiaryPhoneNumber;
+    public Button returnButton;
+    public Button nextButton;
+    public Scene Scene1;
+    public Stage stage;
+    public Parent root;
+
+    public void switchToScene2(ActionEvent event){
+
+    }
+}
