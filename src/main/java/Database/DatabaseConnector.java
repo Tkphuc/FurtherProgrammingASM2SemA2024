c69adcdd3d01ffc38172e3fd85bda9271482dda8
Quanoscar09 <S3975128@rmit.edu.vn>
2024-05-10 18:05:00 +0700

    database

---
+++ src/main/java/Database/DatabaseConnector.java
@@ -0,0 +1,26 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:5432/postgres";
+    private static final String USER = "postgres.iksjbzekgggvbnrryrvu";
+    private static final String PASSWORD = "FPASM2SemA2024";
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        connect();
+    }
+}
